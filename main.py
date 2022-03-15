8ebd5d64e18c3e5b66fd44c2bb2f7c7ece9e5fe4
Waytaria <45755496+Waytaria@users.noreply.github.com>
2022-03-15 08:38:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, request
+app = Flask('app')
+
+classes = [
+  'Guerrier',
+  'Paladin',
+  'Roublard',
+  'Voleur',
+  'Mage',
+  'Archer'
+]
+
+personnages = []
+
+@app.route('/')
+@app.route('/main')
+def main():
+  return render_template('main.html', classes = classes)
+
+@app.route('/liste')
+def liste():
+  global personnages
+  return render_template('liste.html', liste = personnages)
+
+@app.route('/ficheDepuisListe', methods = ['POST'])
+def ficheDepuisListe() :
+  perso = None
+  for e in personnages :
+    if e['nom'] == request.form["nom"] :
+      perso = e
+      break
+  print(perso)
+  return afficherFiche(perso)
+
+@app.route('/fiche', methods = ['POST'])
+def fiche() :
+  dico = {key : value for key, value in request.form.items()}
+  return afficherFiche(dico)
+
+def afficherFiche(perso) :
+  return render_template('fiche.html', perso = perso)
+
+@app.route('/nouveau')
+def nouveau():
+  return render_template('nouveau.html', classes = classes)
+
+@app.route('/ajouter', methods = ['POST'])
+def ajouter() :
+  global personnages
+  dico = {}
+  for key, value in request.form.items() :
+    dico[key] = value
+  personnages += [dico]
+  return afficherFiche(dico)
+
+app.run(host='0.0.0.0', port=8080)
